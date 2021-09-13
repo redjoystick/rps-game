1530d010fc8647406ae35b254c98ab321f54f964
Puzz Chang <espirit.mail@gmail.com>
2021-09-13 22:41:24 +0800

    CSS Layout 示範、querySelector() 與滑鼠點擊事件示範

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -1,3 +1,22 @@
 "use strict";
-console.log("hello world");
+const player1 = {
+    result: document.querySelector("#box #player1-result"),
+    score: document.querySelector("#box #player1-score .point"),
+    cardsBox: document.querySelector("#box #player1-cards"),
+    cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")),
+};
+const rps = ["rock", "paper", "scissors"];
+const result = rps[(Math.random() * rps.length) >> 0];
+// player1.result.style.backgroundImage = `url(./assets/images/${ result }.png)`;
+setRpsIcon(player1.result, result);
+player1.score.textContent = "5";
+// player1.cards[0].style.backgroundImage = `url(./assets/images/${ result }.png)`;
+setRpsIcon(player1.cards[0], result);
+function setRpsIcon(element, rps) {
+    element.style.backgroundImage = `url(./assets/images/${rps}.png)`;
+}
+player1.cardsBox.addEventListener("click", (event) => {
+    const index = player1.cards.indexOf(event.target);
+    console.log(index);
+});
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-console.log("hello world");
+type RPS = "rock" | "paper" | "scissors";
+
+const player1 = {
+    result: document.querySelector("#box #player1-result") as HTMLDivElement,
+    score: document.querySelector("#box #player1-score .point") as HTMLDivElement,
+    cardsBox: document.querySelector("#box #player1-cards") as HTMLDivElement,
+    cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")) as HTMLDivElement[],
+};
+
+const rps = ["rock", "paper", "scissors"] as RPS[];
+
+const result = rps[(Math.random() * rps.length) >> 0];
+// player1.result.style.backgroundImage = `url(./assets/images/${ result }.png)`;
+setRpsIcon(player1.result, result);
+
+player1.score.textContent = "5";
+
+// player1.cards[0].style.backgroundImage = `url(./assets/images/${ result }.png)`;
+setRpsIcon(player1.cards[0], result);
+
+function setRpsIcon(element: HTMLDivElement, rps: RPS) {
+    element.style.backgroundImage = `url(./assets/images/${ rps }.png)`;
+}
+
+player1.cardsBox.addEventListener("click", (event: MouseEvent) => {
+    const index = player1.cards.indexOf(event.target as HTMLDivElement);
+    console.log(index);
+});
