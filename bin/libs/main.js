cd8fde6b76a205efd01d10e238173e0f4f510bf0
DESKTOP-TQ4CDQS\RED <redjoystickmk2@gmail.com>
2021-10-04 02:00:32 +0800

    1004

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -5,18 +5,25 @@ const player1 = {
     cardsBox: document.querySelector("#box #player1-cards"),
     cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")),
 };
+const player2 = {
+    result: document.querySelector("#box #player2-result"),
+    score: document.querySelector("#box #player2-score .point"),
+    cardsBox: document.querySelector("#box #player2-cards"),
+    cards: Array.from(document.querySelectorAll("#box #player2-cards .card-item")),
+};
 const rps = ["rock", "paper", "scissors"];
 const result = rps[(Math.random() * rps.length) >> 0];
 // player1.result.style.backgroundImage = `url(./assets/images/${ result }.png)`;
-setRpsIcon(player1.result, result);
-player1.score.textContent = "5";
+//setRpsIcon(player2.result, result);
+//player1.score.textContent = "5";
 // player1.cards[0].style.backgroundImage = `url(./assets/images/${ result }.png)`;
 setRpsIcon(player1.cards[0], result);
 function setRpsIcon(element, rps) {
-    element.style.backgroundImage = `url(./assets/images/${rps}.png)`;
+    element.style.backgroundImage = `url(./assets/images/card_${rps}.png)`;
 }
 player1.cardsBox.addEventListener("click", (event) => {
     const index = player1.cards.indexOf(event.target);
     console.log(index);
+    setRpsIcon(player1.result, result);
 });
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,32 @@ const player1 = {
     cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")) as HTMLDivElement[],
 };
 
+const player2 = {
+    result: document.querySelector("#box #player2-result") as HTMLDivElement,
+    score: document.querySelector("#box #player2-score .point") as HTMLDivElement,
+    cardsBox: document.querySelector("#box #player2-cards") as HTMLDivElement,
+    cards: Array.from(document.querySelectorAll("#box #player2-cards .card-item")) as HTMLDivElement[],
+};
+
 const rps = ["rock", "paper", "scissors"] as RPS[];
 
 const result = rps[(Math.random() * rps.length) >> 0];
 // player1.result.style.backgroundImage = `url(./assets/images/${ result }.png)`;
-setRpsIcon(player1.result, result);
+//setRpsIcon(player2.result, result);
 
-player1.score.textContent = "5";
+//player1.score.textContent = "5";
 
 // player1.cards[0].style.backgroundImage = `url(./assets/images/${ result }.png)`;
 setRpsIcon(player1.cards[0], result);
 
+
 function setRpsIcon(element: HTMLDivElement, rps: RPS) {
-    element.style.backgroundImage = `url(./assets/images/${ rps }.png)`;
+    element.style.backgroundImage = `url(./assets/images/card_${ rps }.png)`;
 }
 
 player1.cardsBox.addEventListener("click", (event: MouseEvent) => {
     const index = player1.cards.indexOf(event.target as HTMLDivElement);
     console.log(index);
+    setRpsIcon(player1.result, result);
+
 });
