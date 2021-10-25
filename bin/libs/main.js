efea5d00e8ca4ac10fea706e62079e2726ddb37b
DESKTOP-TQ4CDQS\RED <redjoystickmk2@gmail.com>
2021-10-25 20:31:02 +0800

    1025 上傳

--- bin/libs/main.js
+++ bin/libs/main.js
@@ -13,24 +13,45 @@ const player1 = {
     score: document.querySelector("#box #player1-score .point"),
     cardsBox: document.querySelector("#box #player1-cards"),
     cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")),
+    suit: 0,
+    hp: 10,
 };
 const player2 = {
     result: document.querySelector("#box #player2-result"),
     score: document.querySelector("#box #player2-score .point"),
     cardsBox: document.querySelector("#box #player2-cards"),
     cards: Array.from(document.querySelectorAll("#box #player2-cards .card-item")),
+    suit: 0,
+    hp: 10,
 };
 const playerCards = [];
 const cpuCards = [];
-for (let i = 5 - 1; i >= 0; i--) {
-    playerCards[i] = Math.random() * MAX_RPS >> 0;
-    setRpsIcon(player1.cards[i], playerCards[i]);
-    //TODO 完成 cpuCards 
-    //TODO 保底機制: 至少有兩種拳型
+console.log(playerCards);
+console.log(cpuCards);
+function deal() {
+    for (let i = 5 - 1; i >= 0; i--) {
+        playerCards[i] = Math.random() * MAX_RPS >> 0;
+        setRpsIcon(player1.cards[i], playerCards[i]);
+    }
 }
+;
+function dealcpu() {
+    for (let i = 5 - 1; i >= 0; i--) {
+        cpuCards[i] = Math.random() * MAX_RPS >> 0;
+        setRpsIcon(player2.cards[i], cpuCards[i]);
+    }
+}
+;
+deal();
+dealcpu();
+//TODO 保底機制: 至少有兩種拳型
+//TODO 完成 cpuCards 
 // const results = new Array(5).fill("").map(() => Math.random() * MAX_RPS >> 0);
 function setRpsIcon(element, rps, hover = false) {
     element.style.backgroundImage = `url(./assets/images/card_${RPS[rps].toLowerCase()}${hover ? "_s" : ""}.png)`;
+}
+function setHp(element, i) {
+    element.style.backgroundImage = `url(./assets/images/${i}.png)`;
 }
 player1.cardsBox.addEventListener("mouseover", (event) => {
     const i = player1.cards.indexOf(event.target);
@@ -45,15 +66,79 @@ player1.cardsBox.addEventListener("mouseout", (event) => {
 });
 player1.cardsBox.addEventListener("click", (event) => {
     const i = player1.cards.indexOf(event.target);
-    if (i >= 0)
+    if (i >= 0) {
         setRpsIcon(player1.result, playerCards[i]);
+        player1.suit = playerCards[i];
+        playerCards[i] = Math.random() * MAX_RPS >> 0;
+    }
+    console.log(playerCards[i]);
     //TODO 電腦出拳，AI 要多聰明你決定 ?
     ai();
     //TODO 輸贏判斷
     win();
+    setHp(player1.score, player1.hp);
+    setHp(player2.score, player2.hp);
 });
 function ai() {
+    const i = Math.random() * 4 >> 0;
+    player2.suit = cpuCards[i];
+    setRpsIcon(player2.result, cpuCards[i]);
+    cpuCards[i] = Math.random() * MAX_RPS >> 0;
+    setRpsIcon(player2.cards[i], cpuCards[i]);
 }
+;
 function win() {
+    switch (player1.suit) {
+        case 0:
+            if (player2.suit == 0) {
+                console.log("平手" + `${player1.hp = player1.hp}`);
+            }
+            ;
+            if (player2.suit == 1) {
+                player1.hp = player1.hp - 1;
+                console.log("輸" + `${player1.hp}`);
+            }
+            ;
+            if (player2.suit == 2) {
+                player2.hp = player2.hp - 1;
+                console.log("贏" + `${player1.hp}`);
+            }
+            ;
+            break;
+        case 1:
+            if (player2.suit == 0) {
+                player2.hp = player2.hp - 1;
+                console.log("贏" + `${player1.hp}`);
+            }
+            ;
+            if (player2.suit == 1) {
+                console.log("平手" + `${player1.hp}`);
+            }
+            ;
+            if (player2.suit == 2) {
+                player1.hp = player1.hp - 1;
+                console.log("輸" + `${player1.hp}`);
+            }
+            ;
+            break;
+        case 2:
+            if (player2.suit == 0) {
+                player1.hp = player1.hp - 1;
+                console.log("輸" + `${player1.hp}`);
+            }
+            ;
+            if (player2.suit == 1) {
+                player2.hp = player2.hp - 1;
+                console.log("贏" + `${player1.hp}`);
+            }
+            ;
+            if (player2.suit == 2) {
+                console.log("平手" + `${player1.hp}`);
+            }
+            ;
+            break;
+    }
+    //0: 'Rock', 1: 'Paper', 2: 'Scissors'
 }
+;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ const player1 = {
     score: document.querySelector("#box #player1-score .point") as HTMLDivElement,
     cardsBox: document.querySelector("#box #player1-cards") as HTMLDivElement,
     cards: Array.from(document.querySelectorAll("#box #player1-cards .card-item")) as HTMLDivElement[],
+    suit:0,
+    hp:10,
 };
 
 const player2 = {
@@ -18,25 +20,45 @@ const player2 = {
     score: document.querySelector("#box #player2-score .point") as HTMLDivElement,
     cardsBox: document.querySelector("#box #player2-cards") as HTMLDivElement,
     cards: Array.from(document.querySelectorAll("#box #player2-cards .card-item")) as HTMLDivElement[],
+    suit:0,
+    hp:10,
 };
 
 const playerCards = [] as RPS[];
 const cpuCards = [] as RPS[];
+console.log(playerCards);
+console.log(cpuCards);
 
-for (let i = 5 - 1; i >= 0; i--) {
-    playerCards[i] = Math.random() * MAX_RPS >> 0;
-    setRpsIcon(player1.cards[i], playerCards[i]);
+function deal(){
+    for (let i = 5 - 1; i >= 0; i--) {
+        playerCards[i] = Math.random() * MAX_RPS >> 0;
+        setRpsIcon(player1.cards[i], playerCards[i]);
+        
+    }
+};
 
-    //TODO 完成 cpuCards 
+function dealcpu(){
+    for (let i = 5 - 1; i >= 0; i--) {
+        cpuCards[i] = Math.random() * MAX_RPS >> 0;
+        setRpsIcon(player2.cards[i], cpuCards[i]);
+    }
+};
 
-    //TODO 保底機制: 至少有兩種拳型
-}
+deal();
+dealcpu();
+
+//TODO 保底機制: 至少有兩種拳型
+//TODO 完成 cpuCards 
+
 // const results = new Array(5).fill("").map(() => Math.random() * MAX_RPS >> 0);
 
 function setRpsIcon(element: HTMLDivElement, rps: RPS, hover = false) {
     element.style.backgroundImage = `url(./assets/images/card_${ RPS[rps].toLowerCase() }${ hover ? "_s" : "" }.png)`;
 }
 
+function setHp(element: HTMLDivElement, i:number) {
+    element.style.backgroundImage = `url(./assets/images/${i}.png)`;
+}
 
 player1.cardsBox.addEventListener("mouseover", (event: MouseEvent) => {
     const i = player1.cards.indexOf(event.target as HTMLDivElement);
@@ -56,20 +78,51 @@ player1.cardsBox.addEventListener("mouseout", (event: MouseEvent) => {
 player1.cardsBox.addEventListener("click", (event: MouseEvent) => {
     const i = player1.cards.indexOf(event.target as HTMLDivElement);
 
-    if (i >= 0)
-        setRpsIcon(player1.result, playerCards[i]);
+    if ( i>= 0){ 
+        setRpsIcon(player1.result, playerCards[i]); 
+        player1.suit = playerCards[i];
+        playerCards[i] = Math.random() * MAX_RPS >> 0;
+        
+    }
+    
+   
+    console.log(playerCards[i]);  
 
     //TODO 電腦出拳，AI 要多聰明你決定 ?
-    ai()
-
+    ai();
+   
     //TODO 輸贏判斷
-    win()
+    win();
+    setHp(player1.score,player1.hp);
+    setHp(player2.score,player2.hp);
 });
 
 function ai() {
-
-}
+    const i = Math.random() * 4 >> 0;
+    player2.suit = cpuCards[i];
+    setRpsIcon(player2.result, cpuCards[i]);
+    cpuCards[i] = Math.random() * MAX_RPS >> 0;
+    setRpsIcon(player2.cards[i], cpuCards[i]);
+  
+};
 
 function win() {
-
-}
+    switch (player1.suit){
+        case 0 : 
+            if(player2.suit == 0){console.log("平手"+`${player1.hp=player1.hp}`);};
+            if(player2.suit == 1){player1.hp=player1.hp-1;console.log("輸"+`${player1.hp}`);};
+            if(player2.suit == 2){player2.hp=player2.hp-1;console.log("贏"+`${player1.hp}`);};
+            break;
+        case 1 :
+            if(player2.suit == 0){player2.hp=player2.hp-1;console.log("贏"+`${player1.hp}`);};
+            if(player2.suit == 1){console.log("平手"+`${player1.hp}`);};
+            if(player2.suit == 2){player1.hp=player1.hp-1;console.log("輸"+`${player1.hp}`);};
+            break;
+        case 2:
+            if(player2.suit == 0){player1.hp=player1.hp-1;console.log("輸"+`${player1.hp}`);};
+            if(player2.suit == 1){player2.hp=player2.hp-1;console.log("贏"+`${player1.hp}`);};
+            if(player2.suit == 2){console.log("平手"+`${player1.hp}`);};
+            break;
+    }
+//0: 'Rock', 1: 'Paper', 2: 'Scissors'
+};
